a36abbea81aa2ec11d94d51ad0fc33bece73f152
yza9000 <jamaljacks620@gmail.com>
2024-08-03 14:53:35 -0600

    Update kavo.lua

--- libs/kavo.lua
+++ libs/kavo.lua
@@ -12,7 +12,7 @@ function Kavo:DraggingEnabled(frame, parent)
     parent = parent or frame
     
     -- stolen from wally or kiriot, kek
-    local dragging = false
+    local dragging = true
     local dragInput, mousePos, framePos
 
     frame.InputBegan:Connect(function(input)
